5a32d15e5612ae2b414bd1e5b0ec6de4a95c5c07
José Quintas <jose.quintas@lepaya.com>
2022-03-24 00:03:13 +0100

    feat: improve and test shape converter

--- lib/shape_converter.dart
+++ lib/shape_converter.dart
@@ -5,25 +5,51 @@ import 'package:xml/xml.dart';
 class ShapeConverter {
   static const double _kappa = 0.5522847498307935;
 
-  static double c(double number) {
-    return double.parse(number.toStringAsPrecision(12));
+  static double c(double number) =>
+      double.parse(number.toStringAsPrecision(12));
+
+  static double stod(String string) => c(double.parse(string));
+
+  static XmlElement buildPath(
+    String path,
+    List<XmlAttribute> attributes,
+    List<String> filterAttributes,
+  ) =>
+      XmlElement(
+        XmlName('path'),
+        [
+          ...attributes
+              .where(
+                (attribute) => !filterAttributes.contains(attribute.name.local),
+              )
+              .map((e) => e.copy())
+              .toList(),
+          XmlAttribute(XmlName('d'), path),
+        ],
+      );
+
+  static XmlElement lineToPath(XmlElement lineTag) {
+    final filterAttributes = ['x1', 'x2', 'y1', 'y2'];
+    final x1 = stod(lineTag.getAttribute('x1') ?? '0');
+    final y1 = stod(lineTag.getAttribute('y1') ?? '0');
+    final x2 = stod(lineTag.getAttribute('x2') ?? '0');
+    final y2 = stod(lineTag.getAttribute('y2') ?? '0');
+    return buildPath(
+      'M $x1 $y1 L $x2 $y2',
+      lineTag.attributes,
+      filterAttributes,
+    );
   }
 
-  static String lineToPath(XmlElement lineTag) {
-    final x1 = lineTag.getAttribute('x1');
-    final x2 = lineTag.getAttribute('x2');
-    final y1 = lineTag.getAttribute('y1');
-    final y2 = lineTag.getAttribute('y2');
-    return 'M $x1 $y1 L $x2 $y2';
-  }
+  static XmlElement rectToPath(XmlElement rectTag) {
+    final filterAttributes = ['x', 'y', 'width', 'height', 'rx', 'ry'];
 
-  static String rectToPath(XmlElement rectTag) {
-    final x = double.parse(rectTag.getAttribute('x') ?? '0');
-    final y = double.parse(rectTag.getAttribute('y') ?? '0');
-    final w = double.parse(rectTag.getAttribute('width') ?? '0');
-    final h = double.parse(rectTag.getAttribute('height') ?? '0');
-    var rx = double.parse(rectTag.getAttribute('rx') ?? '0');
-    var ry = double.parse(rectTag.getAttribute('ry') ?? '0');
+    final x = stod(rectTag.getAttribute('x') ?? '0');
+    final y = stod(rectTag.getAttribute('y') ?? '0');
+    final w = stod(rectTag.getAttribute('width') ?? '0');
+    final h = stod(rectTag.getAttribute('height') ?? '0');
+    var rx = stod(rectTag.getAttribute('rx') ?? '0');
+    var ry = stod(rectTag.getAttribute('ry') ?? '0');
     final r = c(x + w);
     final b = c(y + h);
 
@@ -34,55 +60,77 @@ class ShapeConverter {
     }
 
     if (rx == 0 && ry == 0) {
-      return 'M $x $y H $r V $b H $x V $y Z';
+      return buildPath(
+        'M $x $y H $r V $b H $x V $y Z',
+        rectTag.attributes,
+        filterAttributes,
+      );
     } else {
-      return 'M ${c(x + rx)} $y '
-          'L ${c(r - rx)} $y '
-          'Q $r $y $r ${c(y + ry)} '
-          'L $r ${c(y + h - ry)} '
-          'Q $r $b ${c(r - rx)} $b '
-          'L ${c(x + rx)} $b '
-          'Q $x $b $x ${c(b - ry)} '
-          'L $x ${c(y + ry)} '
-          'Q $x $y ${c(x + rx)} $y '
-          'Z';
+      return buildPath(
+        'M ${c(x + rx)} $y '
+        'L ${c(r - rx)} $y '
+        'Q $r $y $r ${c(y + ry)} '
+        'L $r ${c(y + h - ry)} '
+        'Q $r $b ${c(r - rx)} $b '
+        'L ${c(x + rx)} $b '
+        'Q $x $b $x ${c(b - ry)} '
+        'L $x ${c(y + ry)} '
+        'Q $x $y ${c(x + rx)} $y '
+        'Z',
+        rectTag.attributes,
+        filterAttributes,
+      );
     }
   }
 
-  static String circleToPath(XmlElement circleTag) {
-    final cx = double.parse(circleTag.getAttribute('cx') ?? '0');
-    final cy = double.parse(circleTag.getAttribute('cy') ?? '0');
-    final r = double.parse(circleTag.getAttribute('r') ?? '0');
+  static XmlElement circleToPath(XmlElement circleTag) {
+    final filterAttributes = ['cx', 'cy', 'r'];
+
+    final cx = stod(circleTag.getAttribute('cx') ?? '0');
+    final cy = stod(circleTag.getAttribute('cy') ?? '0');
+    final r = stod(circleTag.getAttribute('r') ?? '0');
     final cd = r * _kappa;
 
-    return 'M $cx ${c(cy - r)} '
-        'C ${c(cx + cd)} ${c(cy - r)} ${c(cx + r)} ${c(cy - cd)} ${c(cx + r)} $cy '
-        'C ${c(cx + r)} ${c(cy + cd)} ${c(cx + cd)} ${c(cy + r)} $cx ${c(cy + r)} '
-        'C ${c(cx - cd)} ${c(cy + r)} ${c(cx - r)} ${c(cy + cd)} ${c(cx - r)} $cy '
-        'C ${c(cx - r)} ${c(cy - cd)} ${c(cx - cd)} ${c(cy - r)} $cx ${c(cy - r)} '
-        'Z';
+    return buildPath(
+      'M $cx ${c(cy - r)} '
+      'C ${c(cx + cd)} ${c(cy - r)} ${c(cx + r)} ${c(cy - cd)} ${c(cx + r)} $cy '
+      'C ${c(cx + r)} ${c(cy + cd)} ${c(cx + cd)} ${c(cy + r)} $cx ${c(cy + r)} '
+      'C ${c(cx - cd)} ${c(cy + r)} ${c(cx - r)} ${c(cy + cd)} ${c(cx - r)} $cy '
+      'C ${c(cx - r)} ${c(cy - cd)} ${c(cx - cd)} ${c(cy - r)} $cx ${c(cy - r)} '
+      'Z',
+      circleTag.attributes,
+      filterAttributes,
+    );
   }
 
-  static String ellipseToPath(XmlElement ellipseTag) {
-    final cx = double.parse(ellipseTag.getAttribute('cx') ?? '0');
-    final cy = double.parse(ellipseTag.getAttribute('cy') ?? '0');
-    final rx = double.parse(ellipseTag.getAttribute('rx') ?? '0');
-    final ry = double.parse(ellipseTag.getAttribute('ry') ?? '0');
+  static XmlElement ellipseToPath(XmlElement ellipseTag) {
+    final filterAttributes = ['cx', 'cy', 'rx', 'ry'];
+
+    final cx = stod(ellipseTag.getAttribute('cx') ?? '0');
+    final cy = stod(ellipseTag.getAttribute('cy') ?? '0');
+    final rx = stod(ellipseTag.getAttribute('rx') ?? '0');
+    final ry = stod(ellipseTag.getAttribute('ry') ?? '0');
     final cdx = rx * _kappa;
     final cdy = ry * _kappa;
 
-    return 'M $cx ${c(cy - ry)} '
-        'C ${c(cx + cdx)} ${c(cy - ry)} ${c(cx + rx)} ${c(cy - cdy)} ${c(cx + rx)} $cy '
-        'C ${c(cx + rx)} ${c(cy + cdy)} ${c(cx + cdx)} ${c(cy + ry)} $cx ${c(cy + ry)} '
-        'C ${c(cx - cdx)} ${c(cy + ry)} ${c(cx - rx)} ${c(cy + cdy)} ${c(cx - rx)} $cy '
-        'C ${c(cx - rx)} ${c(cy - cdy)} ${c(cx - cdx)} ${c(cy - ry)} $cx ${c(cy - ry)} '
-        'Z';
+    return buildPath(
+      'M $cx ${c(cy - ry)} '
+      'C ${c(cx + cdx)} ${c(cy - ry)} ${c(cx + rx)} ${c(cy - cdy)} ${c(cx + rx)} $cy '
+      'C ${c(cx + rx)} ${c(cy + cdy)} ${c(cx + cdx)} ${c(cy + ry)} $cx ${c(cy + ry)} '
+      'C ${c(cx - cdx)} ${c(cy + ry)} ${c(cx - rx)} ${c(cy + cdy)} ${c(cx - rx)} $cy '
+      'C ${c(cx - rx)} ${c(cy - cdy)} ${c(cx - cdx)} ${c(cy - ry)} $cx ${c(cy - ry)} '
+      'Z',
+      ellipseTag.attributes,
+      filterAttributes,
+    );
   }
 
-  static String? polygonToPath(
+  static XmlElement polygonToPath(
     XmlElement polylineTag, {
     bool isPolyline = false,
   }) {
+    final filterAttributes = ['points'];
+
     final points = polylineTag.getAttribute('points');
     final pointsArrayRaw = points != null ? points.split(',') : <String>[];
     final pointsArray = <String>[];
@@ -100,15 +148,19 @@ class ShapeConverter {
       final buffer = StringBuffer();
       for (var i = 0; i < pointsArray.length; i += 2) {
         buffer
-          ..write(i == 0 ? 'M ' : 'L ')
-          ..write('${pointsArray[i]} ${pointsArray[i + 1]}');
+          ..write(i == 0 ? 'M ' : ' L ')
+          ..write('${stod(pointsArray[i])} ${stod(pointsArray[i + 1])}');
       }
       if (!isPolyline) {
-        buffer.write('Z');
+        buffer.write(' Z');
       }
-      return buffer.toString();
+      return buildPath(
+        buffer.toString(),
+        polylineTag.attributes,
+        filterAttributes,
+      );
     } else {
-      return null;
+      return XmlElement(XmlName('g'));
     }
   }
 }
---
+++ test/shape_converter_test.dart
@@ -0,0 +1,142 @@
+import 'package:svg_to_vector_drawable/shape_converter.dart';
+import 'package:test/test.dart';
+import 'package:xml/xml.dart';
+
+void main() {
+  group('ShapeConverter.lineToPath', () {
+    test('converts a line', () {
+      final element = XmlDocument.parse(
+        '<line x1="0" y1="80" x2="100" y2="20" />',
+      );
+      final result = ShapeConverter.lineToPath(element.getElement('line')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 0.0 80.0 L 100.0 20.0"/>',
+      );
+    });
+
+    test('converts a line and maintains attributes', () {
+      final element = XmlDocument.parse(
+        '<line x1="0" y1="80" x2="100" y2="20" fill="blue" />',
+      );
+      final result = ShapeConverter.lineToPath(element.getElement('line')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path fill="blue" d="M 0.0 80.0 L 100.0 20.0"/>',
+      );
+    });
+  });
+  group('ShapeConverter.rectToPath', () {
+    test('converts a rect with no x & y', () {
+      final element = XmlDocument.parse('<rect width="100" height="100" />');
+      final result = ShapeConverter.rectToPath(element.getElement('rect')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 0.0 0.0 H 100.0 V 100.0 H 0.0 V 0.0 Z"/>',
+      );
+    });
+
+    test('converts a rect with x & y', () {
+      final element =
+          XmlDocument.parse('<rect width="100" height="100" x="120" y="10" />');
+      final result = ShapeConverter.rectToPath(element.getElement('rect')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 120.0 10.0 H 220.0 V 110.0 H 120.0 V 10.0 Z"/>',
+      );
+    });
+
+    test('converts a rect with rounded corners rx', () {
+      final element = XmlDocument.parse(
+        '<rect width="100" height="100" rx="15" />',
+      );
+      final result = ShapeConverter.rectToPath(element.getElement('rect')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 15.0 0.0 L 85.0 0.0 Q 100.0 0.0 100.0 15.0 L 100.0 85.0 Q 100.0 100.0 85.0 100.0 L 15.0 100.0 Q 0.0 100.0 0.0 85.0 L 0.0 15.0 Q 0.0 0.0 15.0 0.0 Z"/>',
+      );
+    });
+
+    test('converts a rect with rounded corners ry', () {
+      final element = XmlDocument.parse(
+        '<rect width="100" height="100" ry="15" />',
+      );
+      final result = ShapeConverter.rectToPath(element.getElement('rect')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 15.0 0.0 L 85.0 0.0 Q 100.0 0.0 100.0 15.0 L 100.0 85.0 Q 100.0 100.0 85.0 100.0 L 15.0 100.0 Q 0.0 100.0 0.0 85.0 L 0.0 15.0 Q 0.0 0.0 15.0 0.0 Z"/>',
+      );
+    });
+
+    test('converts a rect with rounded corners ry & rx', () {
+      final element = XmlDocument.parse(
+        '<rect width="100" height="100" ry="15" rx="5" />',
+      );
+      final result = ShapeConverter.rectToPath(element.getElement('rect')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 5.0 0.0 L 95.0 0.0 Q 100.0 0.0 100.0 15.0 L 100.0 85.0 Q 100.0 100.0 95.0 100.0 L 5.0 100.0 Q 0.0 100.0 0.0 85.0 L 0.0 15.0 Q 0.0 0.0 5.0 0.0 Z"/>',
+      );
+    });
+  });
+
+  group('ShapeConverter.circleToPath', () {
+    test('converts a circle', () {
+      final element = XmlDocument.parse('<circle cx="50" cy="50" r="50"/>');
+      final result = ShapeConverter.circleToPath(element.getElement('circle')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 50.0 0.0 C 77.6142374915 0.0 100.0 22.3857625085 100.0 50.0 C 100.0 77.6142374915 77.6142374915 100.0 50.0 100.0 C 22.3857625085 100.0 0.0 77.6142374915 0.0 50.0 C 0.0 22.3857625085 22.3857625085 0.0 50.0 0.0 Z"/>',
+      );
+    });
+  });
+
+  group('ShapeConverter.ellipseToPath', () {
+    test('converts an ellipse', () {
+      final element =
+          XmlDocument.parse('<ellipse cx="100" cy="50" rx="100" ry="50" />');
+      final result =
+          ShapeConverter.ellipseToPath(element.getElement('ellipse')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 100.0 0.0 C 155.228474983 0.0 200.0 22.3857625085 200.0 50.0 C 200.0 77.6142374915 155.228474983 100.0 100.0 100.0 C 44.7715250169 100.0 0.0 77.6142374915 0.0 50.0 C 0.0 22.3857625085 44.7715250169 0.0 100.0 0.0 Z"/>',
+      );
+    });
+  });
+
+  group('ShapeConverter.polygonToPath', () {
+    test('converts a polygon', () {
+      final element =
+          XmlDocument.parse('<polygon points="0,100 50,25 50,75 100,0" />');
+      final result =
+          ShapeConverter.polygonToPath(element.getElement('polygon')!);
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 0.0 100.0 L 50.0 25.0 L 50.0 75.0 L 100.0 0.0 Z"/>',
+      );
+    });
+    test('converts a polyline', () {
+      final element =
+          XmlDocument.parse('<polyline points="0,100 50,25 50,75 100,0" />');
+      final result = ShapeConverter.polygonToPath(
+        element.getElement('polyline')!,
+        isPolyline: true,
+      );
+      expect(
+        result.toXmlString(pretty: true),
+        '<path d="M 0.0 100.0 L 50.0 25.0 L 50.0 75.0 L 100.0 0.0"/>',
+      );
+    });
+
+    test('returns an empty g if points are wrong', () {
+      final element = XmlDocument.parse('<polygon points="0,100 50" />');
+      final result = ShapeConverter.polygonToPath(
+        element.getElement('polygon')!,
+      );
+      expect(
+        result.toXmlString(pretty: true),
+        '<g/>',
+      );
+    });
+  });
+}
