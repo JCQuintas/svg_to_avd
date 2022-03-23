3339255f632b6a9665d4b303f92d124c85fa2cd9
José Quintas <jose.quintas@lepaya.com>
2022-03-23 22:10:26 +0100

    test: improve coverage of base func

--- test/svg_to_vector_drawable_test.dart
+++ test/svg_to_vector_drawable_test.dart
@@ -1,5 +1,7 @@
+import 'package:svg_to_vector_drawable/errors/invalid_svg_string_exception.dart';
 import 'package:svg_to_vector_drawable/svg_to_vector_drawable.dart';
 import 'package:test/test.dart';
+import 'package:xml/xml.dart';
 
 import 'helpers/helpers.dart';
 
@@ -9,6 +11,19 @@ void main() {
       final result = svgToVectorDrawable(fixture('use.svg'));
 
       expect(result, fixture('use_result.svg'));
+    });
+
+    test('throws a XmlParserException when input is not valid svg or xml', () {
+      void shouldThrow() => svgToVectorDrawable('svg error');
+
+      expect(shouldThrow, throwsA(isA<XmlParserException>()));
+    });
+
+    test('throws an InvalidSvgStringException error when input is not a svg',
+        () {
+      void shouldThrow() => svgToVectorDrawable('<nice></nice>');
+
+      expect(shouldThrow, throwsA(isA<InvalidSvgStringException>()));
     });
   });
 }
